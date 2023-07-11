41223927e3a1fb202df1c4e561bf2527a48fe014
류영찬 (Youngchan Ryu) <youngchanryu@snu.ac.kr>
2023-07-11 19:25:57 +0900

    튜토리얼 part 7, 8 완성
    튜토리얼 끝!

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -38,9 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,7 +61,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -10,6 +11,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean = True,
+        ordering = "pub_date",
+        description = "Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= now - datetime.timedelta(days = 1)
