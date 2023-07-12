506dd22cb4f2f1e6d35af19e34eb049e33bb76aa
류영찬 (Youngchan Ryu) <youngchanryu@snu.ac.kr>
2023-07-12 21:31:52 +0900

    Django REST framework Tutorial 5 완성

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -31,15 +31,19 @@ class SnippetSerializer(serializers.Serializer):
 """
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source = 'owner.username')
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many = True, queryset = Snippet.objects.all())
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,17 @@
 from django.http import Http404
-from rest_framework import status, mixins, generics, permissions
+from django.contrib.auth.models import User\
+
+from rest_framework import status, mixins, generics, permissions, renderers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 
 from .models import Snippet
 from .permission import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
-from django.contrib.auth.models import User
-
-
 # Create your views here.
-
-
 # 그러나 사실은 이렇게 하면 된다..
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -39,6 +38,22 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request = request, format = format),
+        'snippets': reverse('snippet-list', request = request, format = format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 # decorator usage
